641e1ab033ca6d0707bc71998dde661cc0f26b9d
terry <976912095@qq.com>
2021-01-07 17:27:19 +0800

    docs: 归并排序

--- 数据结构与算法/07.sort.js
+++ 数据结构与算法/07.sort.js
@@ -83,3 +83,47 @@ function insertSort(arr) {
     }
     return arr
 }
+
+// 归并排序
+function mergeSort(arr) {
+    const len = arr.length
+    // 处理边界情况
+    if (len <= 1) {
+        return arr
+    }
+    // 计算分割点
+    const mid = Math.floor(len / 2)
+    // 递归分割左子数组，然后合并为有序数组
+    const leftMerge = mergeSort(arr.slice(0, mid))
+    // 递归分割右子数组，然后合并为有序数组
+    const rightMerge = mergeSort(arr.slice(mid, len))
+    // 合并左右两个有序数组
+    arr = mergeArr(leftMerge, rightMerge)
+    return arr
+}
+
+function mergeArr(arr1, arr2) {
+    // 初始化两个指针，分别指向arr1 和 arr2
+    let i = 0, j =0
+    // 初始化结果数组
+    const res = []
+    
+    const len1 = arr1.length
+    const len2 = arr2.length
+    // 合并两个子数组
+    while(i < len1 && j < len2) {
+        if (arr1[i] < arr2[j]) {
+            res.push(arr1[i])
+            i++
+        } else {
+            res.push(arr2[j])
+            j++ 
+        }
+    }
+    // 若其中一个子数组首先被合并完全，则直接拼接另一个子数组的剩余部分
+    if (i < len1) {
+        return res.concat(arr1.slice(i))
+    } else {
+        return res.concat(arr2.slice(j))
+    }
+}
