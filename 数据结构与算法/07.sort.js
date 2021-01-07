0d172d1f6d7b1563a8b306a260eb3f3c40624fde
terry <976912095@qq.com>
2021-01-07 17:03:58 +0800

    docs: 插入排序

--- 数据结构与算法/07.sort.js
+++ 数据结构与算法/07.sort.js
@@ -63,3 +63,23 @@ const arr2 = [2, 3, 6, 4, 1]
 selectSort(arr2)
 
 // 插入排序
+function insertSort(arr) {
+    const len = arr.length
+    // 用temp 来代表当前需要插入的元素
+    let temp
+    // i 用于标识每次被插入的元素的索引
+    for (let i = 1; i < len; i++) {
+        // j 用于帮助 temp 寻找自己应该有的定位
+        let j = i
+        temp = arr[i]
+        // 判断 j 前一个元素是否比 temp 大
+        while (j > 0 && arr[j - 1] > temp) {
+            // 如果是，则将 j 前面的一个元素后移一位， 为temp 让出位置
+            arr[j] = arr[j - 1]      
+            j--
+        }
+        // 循环让位，最后得到的 j 就是 temp 的正确索引
+        arr[j] = temp
+    }
+    return arr
+}
