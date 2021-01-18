92a865f80465c84331ce9e57e663675578104dff
terry <976912095@qq.com>
2021-01-18 09:15:51 +0800

    feat: vue2 父子组件生命周期钩子函数执行顺序

---
+++ 前端面试题/Vue、React、Webpack/vue2-demo/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <!-- {{count}} -->
+    <A :count="count" @add="add"></A>
+  </div>
+</template>
+
+<script>
+import A from "./components/A.vue";
+
+export default {
+  name: "App",
+  components: {
+    A,
+  },
+  data() {
+    return {
+      count: null,
+    };
+  },
+  methods: {
+    add(num) {
+      this.count += num
+    }
+  },  
+  // beforeCreate() {
+  //   console.log("index before create");
+  // },
+  created() {
+    console.log("index created");
+  },
+  // beforeMount() {
+  //   console.log("index before mounte");
+  // },
+  mounted() {
+    console.log("index mounted");
+  },
+  beforeUpdate() {
+    console.log("index before update");
+  },
+  updated() {
+    console.log("index updated");
+  },
+  beforeDestroy() {
+    console.log('index before destory')
+  },
+  destroyed() {
+    console.log('index destoryed')
+  }
+};
+</script>
---
+++ 前端面试题/Vue、React、Webpack/vue2-demo/src/components/A.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    count: {{ count }}
+    <button @click="add">Add One</button>
+    <router-link to="HelloWorld">hello</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "A",
+  props: ["count"],
+  methods: {
+    add() {
+      this.$emit("add", 3);
+    },
+  },
+  // beforeCreate() {
+  //     console.log('A before create')
+  // },
+  created() {
+    console.log("A created");
+  },
+  // beforeMount() {
+  //     console.log('A before mounte')
+  // },
+  mounted() {
+    console.log("A mounted");
+  },
+  beforeUpdate() {
+    console.log("A before update");
+  },
+  updated() {
+    console.log("A updated");
+  },
+  beforeDestroy() {
+    console.log("A before destory");
+  },
+  destroyed() {
+    console.log("A destoryed");
+  },
+};
+</script>
